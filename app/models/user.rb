602a22c9e464d3c6906a23c0a8bdbf30a5e740bf
nagano518 <nt.greeeeeen@gmail.com>
2023-01-20 17:46:57 +0900

    last_name重複削除及び姓名の正規表現指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/, /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/, /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
   has_many :items
