d4947e9553b7eae705248f5e812b19ce38ea7dc3
nagano518 <nt.greeeeeen@gmail.com>
2023-01-20 17:08:15 +0900

    Userモデルの単体テストコードの記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
+    birthday              {Faker::Date.in_date_period}
   end
 end
