847880e25fe22ee6f9d7114ccf982ebe2a935431
nagano518 <nt.greeeeeen@gmail.com>
2023-01-23 18:48:40 +0900

    出品ページへ遷移時のリダイレクト設定、カテゴリーのエラーメッセージ設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :area_id, presence: true
   validates :shipping_date, presence: true
   validates :price, presence: true, format: { with: /\A[0-9]{3,7}+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :genre_id, numericality: { other_than: 1 ,message: "can't be blank"} 
 
   belongs_to :user
   has_one_attaches :image
