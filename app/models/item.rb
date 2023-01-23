73e2c0ccac68610588c2f3e83fa269e1e4937d2b
nagano518 <nt.greeeeeen@gmail.com>
2023-01-23 19:34:35 +0900

    商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,15 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :content, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :area_id, presence: true
-  validates :shipping_date, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
+  validates :status_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
+  validates :area_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
+  validates :shipping_date, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :price, presence: true, format: { with: /\A[0-9]{3,7}+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :genre_id, numericality: { other_than: 1 ,message: "can't be blank"} 
 
   belongs_to :user
-  has_one_attaches :image
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
--- app/models/shipping_date.rb
+++ app/models/shipping_date.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class Shipping_date < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '1\~2日で発送' },
