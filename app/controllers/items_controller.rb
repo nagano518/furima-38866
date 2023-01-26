2cfb2798fa7eb48c074ea85a053e0089470581e6
nagano518 <nt.greeeeeen@gmail.com>
2023-01-26 16:11:23 +0900

    editコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
