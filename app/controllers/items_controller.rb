ac46f1c87c8ed83c8c9fc3984614dbd3b9a616ba
nagano518 <nt.greeeeeen@gmail.com>
2023-01-26 15:54:24 +0900

    updateコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    if @item.update
+    if item.update(item_params)
       redirect_to item_path
     else
       render :show
