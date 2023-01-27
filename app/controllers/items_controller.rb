1ebed3772e264ccf23661ff0f23c4b06bf087d5a
nagano518 <nt.greeeeeen@gmail.com>
2023-01-27 17:14:25 +0900

    条件分岐をbefore_actionに設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :user_choice, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -37,10 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
-
     if @item.destroy
       redirect_to root_path
     end
@@ -54,5 +48,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def user_choice
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
