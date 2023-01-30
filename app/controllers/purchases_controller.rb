c22b414c58356acece8ae9cd15abecd44722fec9
nagano518 <nt.greeeeeen@gmail.com>
2023-01-31 00:10:12 +0900

    Formオブジェクトを用いて、複数のテーブルに情報を保存

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,26 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    @form_object = FormObject.new
+  end
+
+  def create
+    binding.pry
+    @form_object = FormObject.new(object_params)
+    if @form_object.valid?
+      @form_object.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def object_params
+    item_id = Item.find(params[:item_id]).id
+    params.require(:form_object).permit(:postal_code, :area_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: item_id)
+  end
+end
--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -1,19 +1,19 @@
 class FormObject
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :area_id, :municipality_id, :address, :building_name, :telephone_number, :purchase 
+  attr_accessor :item_id, :user_id, :postal_code, :area_id, :municipality, :address, :building_name, :telephone_number, :purchase_id 
 
-  validates :item, presence: true
-  validates :user, presence: true
+  validates :item_id, presence: true
+  validates :user_id, presence: true
   validates :postal_code, presence: true, format: { with:/\A\d{3}[-]\d{4}\z/ }
   validates :area_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
   validates :municipality, presence: true
   validates :address, presence: true
-  validates :building_name
-  validates :telephone, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :purchase, presence: true
+  validate :building_name
+  validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    address = Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, telephone: telephone, purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
